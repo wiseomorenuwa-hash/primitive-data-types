5965b0937797da8bddd7046d0c0d0933fa527363
wise <wiseomorenuwa@gmail.com>
2026-08-16 21:10:07 +0100

    completed booleans lessons

--- booleans/booleans.js
+++ booleans/booleans.js
@@ -6,7 +6,7 @@
 //
 ////////////////////// YOUR CODE BELOW THIS LINE ///////////////////////
 
-let newBoolean
+let newBoolean = true
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -25,9 +25,9 @@ let dadsHeight = generateRandomNumber(100, 200)
 //
 ////////////////////// YOUR CODE BELOW THIS LINE ///////////////////////
 
-let momIsTaller
-let dadIsTaller
-let areTheSameHeight
+let momIsTaller = momsHeight > dadsHeight
+let dadIsTaller = dadsHeight > momsHeight
+let areTheSameHeight = dadsHeight == momsHeight
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -46,7 +46,7 @@ let studentEssay = generateSudentEssay()
 //
 ////////////////////// YOUR CODE BELOW THIS LINE ///////////////////////
 
-let notPlagiarised
+let notPlagiarised = !studentEssay.includes(keyPhrase)
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -68,10 +68,10 @@ let thresholdForTall = 150
 //
 ////////////////////// YOUR CODE BELOW THIS LINE ///////////////////////
 
-let siblingsAreTall
-let siblingsAreShort
-let atLeastOneIsTall
-let onlyOneIsTall
+let siblingsAreTall = sistersHeight > thresholdForTall && brothersHeight > thresholdForTall
+let siblingsAreShort = sistersHeight < thresholdForTall && brothersHeight < thresholdForTall
+let atLeastOneIsTall = sistersHeight > thresholdForTall || brothersHeight > thresholdForTall
+let onlyOneIsTall = !siblingsAreTall && !siblingsAreShort
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -94,7 +94,9 @@ let secretCode = generateSecretCode()
 //
 ////////////////////// YOUR CODE BELOW THIS LINE ///////////////////////
 
-let codeIsValid
+let correct5thchar = secretCode.charAt(4) >= 65 && secretCode.charAt(4) <=90
+let correct10thchar = secretCode.charAt(11) >= 47 && secretCode.charAt(11) <= 56
+let codeIsValid = correct5thchar && correct10thchar
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -115,13 +117,13 @@ let truthy1, truthy2
 //
 ////////////////////// YOUR CODE BELOW THIS LINE ///////////////////////
 
-// falsy1 = Boolean("")
-// falsy2 = Boolean(0)
-// falsy3 = Boolean(undefined)
-// falsy4 = Boolean(null)
+falsy1 = Boolean("")
+ falsy2 = Boolean(0)
+ falsy3 = Boolean(undefined)
+ falsy4 = Boolean(null)
 
-// truthy1 = Boolean("non-empty string")
-// truthy2 = Boolean(1)
+ truthy1 = Boolean("non-empty string")
+ truthy2 = Boolean(1)
 
 ////////////////////////////////////////////////////////////////////////
 
