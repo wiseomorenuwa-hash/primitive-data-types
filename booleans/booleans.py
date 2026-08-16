653c7b1b11b9dd88fd990f06b075f05dddbf6c1e
wise <wiseomorenuwa@gmail.com>
2026-08-16 19:47:50 +0100

    completed booleans exercise

--- booleans/booleans.py
+++ booleans/booleans.py
@@ -6,7 +6,7 @@
 #
 ###################### YOUR CODE BELOW THIS LINE #######################
 
-newBoolean = None
+newBoolean = False
 
 ########################################################################
 
@@ -27,7 +27,7 @@ scoreToPass = 75
 #
 ###################### YOUR CODE BELOW THIS LINE #######################
 
-studentPasses = None
+studentPasses = examScore1 >= 75 or examScore2 >= 75
 
 ########################################################################
 
@@ -58,10 +58,10 @@ student2Chuck = random.randint(4, 6)
 #
 ###################### YOUR CODE BELOW THIS LINE #######################
 
-numberOfWood = None
-numberOfChuck = None
+numberOfWood = tongueTwiser.count('wood ') + tongueTwiser.count('wood,') + tongueTwiser.count('wood.') + tongueTwiser.count('wood?')
+numberOfChuck = tongueTwiser.count(' chuck')
 
-bothAtLeastOneCorrect = None
+bothAtLeastOneCorrect = False
 
 ########################################################################
 
@@ -83,12 +83,12 @@ truthy1 = truthy2 = None
 #
 ###################### YOUR CODE BELOW THIS LINE #######################
 
-# falsy1 = bool("")
-# falsy2 = bool(0)
-# falsy3 = bool(None)
+falsy1 = bool("")
+falsy2 = bool(0)
+falsy3 = bool(None)
 
-# truthy1 = bool("non-empty string")
-# truthy2 = bool(1)
+truthy1 = bool("non-empty string")
+truthy2 = bool(1)
 
 ########################################################################
 
